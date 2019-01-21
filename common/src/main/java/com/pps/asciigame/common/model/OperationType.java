7b9bb795aa4e66bc2a89ec481db6d6078e72ff60
Gekon <olekman13@gmail.com>
2019-01-21 11:08:34 +0100

    Stealing resources

--- common/src/main/java/com/pps/asciigame/common/model/OperationType.java
+++ common/src/main/java/com/pps/asciigame/common/model/OperationType.java
@@ -3,18 +3,31 @@ package com.pps.asciigame.common.model;
 import java.io.Serializable;
 
 public enum OperationType implements Serializable {
-	STEAL_1(5),
-	BURN_1(5),
-	STEAL_2(10),
-	BURN_2(10);
+	STEAL_1(5, "steal", 0.1),
+	BURN_1(5, "burn", 0.1),
+	STEAL_2(10, "steal", 0.2),
+	BURN_2(10, "burn", 0.2);
 	
 	private final int range;
+	private final String effect;
+	private final double percent;
 	
-	OperationType(final int range){
+	OperationType(final int range, final String effect, final double percent){
 		this.range = range;
+		this.effect = effect;
+		this.percent = percent;
 	}
 
 	public int getRange() {
 		return range;
 	}
+
+	public String getEffect() {
+		return effect;
+	}
+
+	public double getPercent() {
+		return percent;
+	}
+	
 }
--- ws/src/main/java/com/pps/asciigame/ws/game/web/OperationsController.java
+++ ws/src/main/java/com/pps/asciigame/ws/game/web/OperationsController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pps.asciigame.common.model.Building;
 import com.pps.asciigame.common.model.Operation;
+import com.pps.asciigame.common.model.Resource;
+import com.pps.asciigame.common.model.ResourceAmounts;
 import com.pps.asciigame.ws.game.bases.BaseService;
+import com.pps.asciigame.ws.game.resources.ResourceService;
 
 public class OperationsController {
 	
 	@Autowired
     private BaseService baseService;
+	@Autowired
+	private ResourceService resourceService;
 	
     public void performOperation(final Operation operation) {
     	final var base = operation.getHomeBase();
@@ -21,6 +26,27 @@ public class OperationsController {
     			// todo - what should it do depending on the operation
     		}
     	}
+    }
+    
+    public void stealResources(final Operation operation) {
+    	final var resourcesTarget = resourceService.getAll(operation.getTargetBase().getOwner());
+    	final var resourcesHome = resourceService.getAll(operation.getHomeBase().getOwner());
+    	
+    	resourcesHome.forEach(resource -> {
+            resource.setAmount(resource.getAmount() + resource.getAmount() * operation.getOperationType().getPercent()); //todo - how to add percent of resources from different player?
+            resourceService.update(resource);
+        });
+    	
+        resourcesTarget.forEach(resource -> {
+            resource.setAmount(resource.getAmount() - resource.getAmount() * operation.getOperationType().getPercent());
+            resourceService.update(resource);
+        });
+        
+        
+    }
+    
+    public void burnBuilding(final Operation operation) {
+    	
     }
     
     public int calculateRange(final Operation operation)
